ca44c669b787f901dee3ead5c3477129a02905ff
dannykay62 <danielkay62@gmail.com>
2023-11-30 04:40:29 +0100

    Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -2,28 +2,39 @@ import kue from 'kue';
 const queue = kue.createQueue();
 
 export default function createPushNotificationsJobs(jobs, queue) {
-    if(!Array.isArray(jobs)) {
-        throw new Error('Jobs is not an array');
+    if (!Array.isArray(jobs)) {
+      throw new Error('Jobs is not an array');
     }
-
-    jobs.forEach(jobData => {
-        const job = queue.create('push_notification_code_3', jobData).save(
-            err => {
-                if (!err) {
-                    console.log(`Notification job created: ${job.id}`);
-                } else {
-                    console.error('Error creating job:', err);
-                }
-            });
-
-            job.on('complete', () => {
-                console.log(`Notification job ${job.id} completed`);
-            });
-            job.on('failed', err => {
-                console.error(`Notification job ${job.id} failed: ${err}`);
-            });
-            job.on('progress', progress => {
-                console.log(`Notification job ${job.id} ${progress}% complete`);
-            });
-    });
-}
+  
+    try {
+      jobs.forEach(jobData => {
+        const job = queue.create('push_notification_code_3', jobData);
+        if (!job) {
+          console.error('Failed to create job');
+          return; // Skip if job creation fails
+        }
+        
+        job.save(err => {
+          if (!err) {
+            console.log(`Notification job created: ${job.id}`);
+          } else {
+            console.error('Error creating job:', err);
+          }
+        });
+  
+        job.on('complete', () => {
+          console.log(`Notification job ${job.id} completed`);
+        });
+  
+        job.on('failed', err => {
+          console.error(`Notification job ${job.id} failed: ${err}`);
+        });
+  
+        job.on('progress', progress => {
+          console.log(`Notification job ${job.id} ${progress}% complete`);
+        });
+      });
+    } catch (error) {
+      console.error('Error creating jobs:', error);
+    }
+  }  
